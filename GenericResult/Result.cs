d6a0a07658b8624ff9c7b2eacf667dea5d277bc5
arrak.rukkharat <arrak.rukkharat@gmail.com>
2020-09-22 22:46:17 -0800

    Initial work

---
+++ GenericResult/Result.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericResult
+{
+    /// <summary>
+    /// A result which contains a value on success or errors on failure
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Result<T> where T : class, new()
+    {
+        /// <summary>
+        /// Gets the value from a successful <see cref="Result{T}"/>
+        /// </summary>
+        public T Value { get; private set; }
+
+        /// <summary>
+        /// Gets a list of error messagages from a failed <see cref="Result{T}"/>
+        /// </summary>
+        public List<string> Errors { get; private set; } = new List<string>();
+
+        /// <summary>
+        /// Gets an error messagages from a failed <see cref="Result{T}"/>
+        /// This returns the first error message if there are multiple.
+        /// </summary>
+        public string Error => Errors.FirstOrDefault();
+
+        /// <summary>
+        /// Indicates that the result has failed
+        /// </summary>
+        public bool IsFailed
+        {
+            get
+            {
+                return Errors != null && Errors.Any();
+            }
+        }
+
+        /// <summary>
+        /// Indicates that the result has succeeded
+        /// </summary>
+        public bool IsSuccessful
+        {
+            get
+            {
+                return Errors == null || !Errors.Any();
+            }
+        }
+
+        /// <summary>
+        /// Creates a successful result with a value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Result<T> Succeeds(T value)
+        {
+            return new Result<T>
+            {
+                Value = value
+            };
+        }
+
+        /// <summary>
+        /// Creates an error result with a list of messages
+        /// </summary>
+        /// <param name="messages"></param>
+        /// <returns></returns>
+        public static Result<T> Fails(List<string> messages)
+        {
+            return new Result<T>
+            {
+                Errors = messages ?? new List<string>()
+            };
+        }
+
+        /// <summary>
+        /// Creates an error result with a message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static Result<T> Fails(string message)
+        {
+            return new Result<T>
+            {
+                Errors = new List<string>()
+                {
+                    message
+                }
+            };
+        }
+    }
+}
